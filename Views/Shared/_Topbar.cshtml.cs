d06260bbcc1ea8ca5dc132de65cd4fe046179891
erickelian <elianlagartija@gmail.com>
2023-01-20 16:41:28 -0500

    nuevo repositorio

---
+++ Views/Shared/_Topbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AplicacionWeb2._0.Views.Shared
+{
+    public class _TopbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
